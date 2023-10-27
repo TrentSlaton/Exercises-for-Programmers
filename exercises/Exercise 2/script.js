717b14d829a69c5da6ce3fa3c24ac74c93d5e73e
TS <golfingtrex@gmail.com>
2023-10-27 02:32:12 -0500

    relative path issue

---
+++ exercises/Exercise 2/script.js
@@ -0,0 +1,6 @@
+// count total characters
+// count punctuation and whitespace separately
+// have options to include/exclude those separate counts
+// support very large inputs, but handle it gracefully
+// good UI
+// histogram of characters (possibly using chart.js)
