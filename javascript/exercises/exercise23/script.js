fa895d733a9973a32e895043d3881283dbaa94e1
TS <golfingtrex@gmail.com>
2024-05-28 22:54:53 -0500

    comment

--- javascript/exercises/exercise23/script.js
+++ javascript/exercises/exercise23/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/*
+
+So I haven't gotten this working yet, but the idea is to implement some functional programming here... I want to initialize the the DOM elements to the initial question and then have event listeners on the yes/no buttons that will call the update DOM function, which will take in the old DOM and return the updated DOM...
+
+Both the "yes" and "no" buttons will call the same function, but with "yes" and "no" parameters respectively. That function will perform a switch or if statement on the last element of the ul and see if it includes the various phrases ("Does the car make a clicking noise?" or "Does your car have fuel injection?") and based on the response of "yes" or "no", it will update the DOM accordingly.
+
+Updating the DOM will be done by taking in the old DOM, making a copy of it, and appending the new question to the copy and returning that.
+
+*/
+
 // get UI elements
 const qna = document.getElementById("qna");
 const qnaList = (() => {
@@ -12,23 +22,30 @@ const qnaList = (() => {
 
     return qnaList;
 })();
-
 const yes = document.getElementById("yes");
 const no = document.getElementById("no");
 
-// get answers
-const getAnswers = (question) => {
+// update qnaList
+const updateqnaList = (oldqnaList, response) => {
     //
+    let newItem;
+    const qnaList = [...oldqnaList];
+    if (oldqnaList[oldqnaList.length - 1].includes("Is the car silent when you turn the key?")) {
+        newItem = "";
+    }
+
+    return [...oldqnaList, newItem];
 };
 
-// add even listeners and update UI elements
+// add event listeners and update UI elements
 yes.addEventListener("click", () => {
     //
-    // console.log(qna[qna.length - 1]);
-    console.log(qnaList);
-    //console.log(qnaList[qnaList.length - 1].innerHTML);
+    // console.log(qnaList[qnaList.length - 1].innerHTML);
+    // qnaList = updateqnaList(qnaList, "yes");
+    updateqnaList(qnaList, "yes");
 });
 
 no.addEventListener("click", () => {
     //
+    updateqnaList(qnaList, "yes");
 });
